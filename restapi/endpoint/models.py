e0902bdeddfc379517d8ed8f17bc5a334d9fddc8
Thomas Wang <dev@paced.me>
2017-07-29 18:32:03 +1000

    Models completed

--- restapi/endpoint/models.py
+++ restapi/endpoint/models.py
@@ -8,11 +8,17 @@ class Hashtag(models.Model):
     tag = models.CharField(max_length=63)
 
 
-class HashtagList(models.Model):
-    """Intermediary model for categorical hashtags."""
-
-
 class Recording(models.Model):
     """Model for recorded files."""
 
+    date = models.DateTimeField(auto_now=True)
     filename = models.CharField(max_length=255)
+    hashtags = models.ManyToManyField(Hashtag)
+
+
+class Emotion(models.Model):
+    """Model for emotions and values."""
+
+    emotion = models.CharField(max_length=63)
+    value = models.IntegerField()
+    recording = models.ForeignKey(Recording, on_delete=models.CASCADE)
