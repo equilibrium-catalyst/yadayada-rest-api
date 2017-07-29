b463e6ac0ae02dda315824651b31008098cf830f
Thomas Wang <dev@paced.me>
2017-07-29 17:17:16 +1000

    REST API set up

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# yadayada-rest-api
+yadayada-rest-api
+=================
+
 Django RESTful backend API for the YadaYada application.
+
+Installation
+------------
+
+Just clone the repository, activate the virtualenv, and install the requirements found in `requirements.txt`. After this, cd into the `restapi` directory and:
+
+```sh
+$ python manage.py migrate
+$ python manage.py createsuperuser
+```
+
+Documentation
+-------------
+
+-	Upload a sound file to the server.
+
+```
+HTTP 1.1 POST
+'apikey': some valid API key
+'file': some sound file
+```
+
+...returns:
+
+```
+JSON
+'sentiment': an object of strings to integers (out of 100)
+'transcript': a long string of the user's speech
+'tags': an array of strings
+```
+
+-	Query the database given hashtag categories.
+
+```
+HTTP 1.1 POST
+'apikey': some valid API key
+'tags': all of the tags that we want to use to rank search
+```
+
+Pipeline
+--------
+
+When the file is uploaded, it is taken through the following pipeline logic:
+
+1.	File is uploaded and now exists in memory.
+2.	File's sentiment (emotions) are analysed and stored in memory in a list.
+3.	File is brought through speech-to-text, then through natural language processing to get the context.
+4.	File is stored on the file system.
+5.	An object is created and placed into the database.
+6.	JSON is returned to the user.
---
+++ restapi/restapi/settings.py
@@ -0,0 +1,116 @@
+"""Django REST api for the YadaYada application."""
+import os
+
+from . import secure
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#
+# Security
+#
+
+SECRET_KEY = secure.SECRET_KEY
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+#
+# Application
+#
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'restapi.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'restapi.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+#
+# Database
+#
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#
+# Passwords
+#
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
+    },
+]
+
+
+#
+# Internationalisation
+#
+
+LANGUAGE_CODE = 'en-au'
+TIME_ZONE = 'Australia/Melbourne'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+#
+# Staticfiles
+#
+
+STATIC_URL = '/static/'
