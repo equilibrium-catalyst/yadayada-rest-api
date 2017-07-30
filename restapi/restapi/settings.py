21ea5409fdab6faed552c178e551f0eee53af3b5
Thomas Wang <dev@paced.me>
2017-07-30 21:31:24 +1000

    Remove unnecessary requirements

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -66,11 +66,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
 }
 
 LOGIN_REDIRECT_URL = "/"
