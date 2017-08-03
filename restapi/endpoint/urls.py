bdd95f358d9a7926c704e8dc2547ca267ff580f5
Thomas Wang <dev@paced.me>
2017-08-03 21:35:59 +1000

    AJAX request now works!

--- restapi/endpoint/urls.py
+++ restapi/endpoint/urls.py
@@ -13,7 +13,7 @@ from django.contrib.auth.models import User
 from pydub import AudioSegment
 from rest_framework import routers, serializers, viewsets
 
-from . import models
+from . import models, views
 
 #
 # Launch Vokaturi
@@ -184,7 +184,8 @@ router.register(r'recordings', RecordingViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework'))
+                               namespace='rest_framework')),
+    url(r'^test/', views.test)
 ]
 
 
